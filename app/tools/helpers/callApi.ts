0751ed87bf9988143be092c730f370eede2f35c4
mohammad amin <man.dehghani1991@gmail.com>
2024-06-08 14:23:53 -0400

    add token to login (it works)

--- app/tools/helpers/callApi.ts
+++ app/tools/helpers/callApi.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 import validationErrors from "@/app/tools/exceptions/validationErrors";
+//import { useRouter } from "next/navigation";
+//import { useRouter } from "next/router";
+
 
 const callApi = () => {
+    //const router = useRouter();
     const axiosInstance = axios.create({
         baseURL: 'http://localhost:3001/api',
         withCredentials: true, // Allow sending cookies with requests
@@ -10,6 +14,12 @@ const callApi = () => {
     axiosInstance.interceptors.request.use(
         (config) => {
             config.withCredentials = true;
+
+            const token = localStorage.getItem('login-token');
+            if (token) {
+              config.headers['Authorization'] = `Bearer ${token}`;
+            }
+
             return config;
         },
         err => { throw err; }
@@ -25,16 +35,49 @@ const callApi = () => {
         },
         err => {
             const res = err?.response
+            // if (res) {
+            //     console.log('error:callApi', res)
+
+            //     // if (res.status === 403) {
+            //     //     router.push('auth/login'); here is good?
+            //     // }
+
+            //     if (res.status === 422 || res.status === 401) {
+            //         throw new validationErrors(res.data.errors)
+            //     }
+            // }
+
+
             if (res) {
-                console.log('error:callApi', res)
-                if (res.status === 422 || res.status === 401) {
-                    throw new validationErrors(res.data.errors)
+                console.log('error:callApi', res);
+
+                // Handle specific status codes
+                switch (res.status) {
+                    case 400:
+                        console.error('Bad Request', res.data);
+                        break;
+                    case 401:
+                        console.error('Unauthorized', res.data);
+                        //router.push('/auth/login'); // Redirect to login page
+                        break;
+                    case 403:
+                        console.error('Forbidden', res.data);
+                        throw new validationErrors(res.data.errors);
+                    case 422:
+                        throw new validationErrors(res.data.errors);
+                    case 500:
+                        console.error('Internal Server Error', res.data);
+                        break;
+                    default:
+                        console.error('An error occurred', res.data);
                 }
+            } else {
+                console.error('An unknown error occurred', err);
             }
 
+
+
             throw err;
-
-
         }
     )
 
